cebbecf9b9901d6d1dc4928a41d794d084360443
KarollinyLino <karollinylino@gmail.com>
2026-08-31 18:45:40 -0300

    Refactor prime number verification code

--- Estrutura_de_Repeticao/src/Vericacao_de_numero_primo.java
+++ Estrutura_de_Repeticao/src/Vericacao_de_numero_primo.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-//ATV_02 Dia 23/08/26
 public class Vericacao_de_numero_primo {
     public static void main (String[] args) {
         Scanner leitor = new Scanner(System.in);
